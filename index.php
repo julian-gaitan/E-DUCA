6733a92a0d7bebf75f8846e449e76f333d2ba904
Julian Gaitan <julian89net@gmail.com>
2023-10-24 23:49:25 -0500

    Se agrega el Navbar con logo y el favicon

--- index.php
+++ index.php
@@ -1,25 +1,71 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-DUCA</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css?<?=time();// Avoids CSS cached problems?>">
 </head>
 
-<body>
-    <h1>Hello</h1>
+<body class="min-vh-100 bg-main-dark text-white">
 
-    
+    <div class="container">
+        <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img class="img-logo-navbar" src="img/logo-navbar.png" alt="logo-navbar">
+                    E-DUCA
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarMain">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cursos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Precios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Testimonios</a>
+                        </li>
+
+                        <!-- <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li> -->
+                    </ul>
+                    <div class="row col-4 ms-lg-auto">
+                        <div class="col-lg-6 mb-3 mb-lg-0">
+                            <button class="btn btn-primary w-100" type="button">Ingreso</button>
+                        </div>
+                        <div class="col-lg-6 mb-2 mb-lg-0">
+                            <button class="btn btn-info w-100" type="button">Registro</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
 
     <!-- <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="js/jquery-3.7.1.min.js"></script>
     <script>
-        $("h1:first-child").text("Hi");
+        // $("h1:first-child").text("Hola");
     </script>
 </body>
 
