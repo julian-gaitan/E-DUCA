e887facc022cfe11c6b0e834f804d08e2bbb34fc
Julian Gaitan <julian89net@gmail.com>
2023-12-04 17:33:37 -0500

    Se agrega la parte de Lectura para el CRUD de Cronogramas

---
+++ lib/schedule.php
@@ -0,0 +1,80 @@
+<?php
+
+include_once 'orm.php';
+
+class Schedule extends ORM {
+
+    protected int $id;
+    protected int $fk_course;
+    protected string $start_date;
+    protected string $end_date;
+    protected int $duration;
+    public const TABLE_NAME = 'tbl_cronogramas';
+    public const FIELDS_MAP = [
+        'id' => 'id',
+        'fk_course' => 'fk_curso',
+        'start_date' => 'fecha_inicio',
+        'end_date' => 'fecha_fin',
+        'duration' => 'duracion',
+    ];
+
+    function __construct() {
+        $this->table_name = self::TABLE_NAME;
+        $this->fields_map = self::FIELDS_MAP;
+        $this->id = 0;
+        $this->fk_course = 0;
+        $this->start_date = "";
+        $this->end_date = "";
+        $this->duration = 0;
+    }
+
+    protected function newObj() {
+        return new Schedule();
+    }
+
+    public function get_id(): int {
+        return $this->id;
+    }
+
+    public function set_id(int $id): void {
+        if (is_null($id)) return;
+        $this->id = $id;
+    }
+
+    public function get_fk_course(): int {
+        return $this->fk_course;
+    }
+
+    public function set_fk_course(int $fk_course): void {
+        if (is_null($fk_course)) return;
+        $this->fk_course = $fk_course;
+    }
+
+    public function get_start_date(): string {
+        return $this->start_date;
+    }
+
+    public function set_start_date(string $start_date): void {
+        if (is_null($start_date)) return;
+        $this->start_date = $start_date;
+    }
+
+    public function get_end_date(): string {
+        return $this->end_date;
+    }
+
+    public function set_end_date(string $end_date): void {
+        if (is_null($end_date)) return;
+        $this->end_date = $end_date;
+    }
+
+    public function get_duration(): int {
+        return $this->duration;
+    }
+
+    public function set_duration(int $duration): void {
+        if (is_null($duration)) return;
+        $this->duration = $duration;
+    }
+
+}
--- manage_schedules.php
+++ manage_schedules.php
@@ -12,7 +12,7 @@ if (!in_array($file_name, $pages_auth)) {
 }
 ?>
 <?php
-
+$content = Schedule::findAll($conn, new Schedule());
 ?>
 
 <?php include "layout/header.php"; ?>
@@ -24,7 +24,35 @@ if (!in_array($file_name, $pages_auth)) {
         </div>
         <div class="row justify-content-center align-items-center flex-grow-1 flex-shrink-0">
             <div class="col-md-12 col-lg-10 col-xl-8">
+                <table class="table table-primary table-striped table-bordered table-hover">
+                    <thead class="table-light text-center">
+                        <tr>
+                            <th>#</th>
+                            <th>Nombre</th>
+                            <th>Fecha Inicio</th>
+                            <th>Fecha Fin</th>
+                            <th colspan="2">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody class="align-middle">
+                        <?php for ($i = 0; $i < count($content); $i++) { ?>
+                            <?php $schedule = $content[$i]; ?>
+                            <?php $course = Course::findbyId($conn, new Course, $schedule->get_fk_course()); ?>
+                            <tr>
+                                <td><?php echo ($i + 1); ?></td>
+                                <td><?php echo $course->get_name(); ?></td>
+                                <td><?php echo $schedule->get_start_date(); ?></td>
+                                <td><?php echo $schedule->get_end_date(); ?></td>
+                                <td><a href="?modify=<?php echo $schedule->get_id(); ?>" class="btn btn-warning">Modificar</a></td>
+                                <td><a href="?delete=<?php echo $schedule->get_id(); ?>" class="btn btn-danger">Eliminar</a></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
             </div>
+        </div>
+        <div class="pt-3 pb-5 flex-grow-0 flex-shrink-0 text-center">
+            <a href="?create" class="btn btn-info btn-lg fw-bold">Nuevo</a>
         </div>
     </div>
 <?php } ?>
